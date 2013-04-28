cd988d0c9d895e881946f04c731f9f54d1dc00fa
Dominik Kundel <dominik.kundel@me.com>
2013-04-28 11:45:14 +0100

    pretty picture

--- coldruins/web/static/js/main.js
+++ coldruins/web/static/js/main.js
@@ -10,7 +10,7 @@ jQuery(function jquery_dom_loaded () {
 window.facebook_logged_in = function facebook_logged_in (authResponse) {
   FB.api('/me?fields=id,first_name,last_name,picture', function facebook_api_callback (response) {
     $('#user').html(response.first_name+' '+response.last_name);
-    $('#user_img').attr('src', response.picture.data.url);
+    $('#user_img img').attr('src', response.picture.data.url);
     for(var i=0; i <= 4; i++) {
       if (window.userresources[i])
         $('output.'+CATEGORIES_MAP[i+1]).html(window.userresources[i]);
